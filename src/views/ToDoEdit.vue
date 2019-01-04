0193beaabe77132fdf4709a6f08a853babc28be6
Nicole Kissell <colekissell@gmail.com>
2019-01-04 01:31:45 -0700

    added basic setup and todolist

---
+++ src/views/ToDoEdit.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is the todo detail page</h1>
+    <!-- <ToDoComp></ToDoComp> -->
+    <CompletedTodos></CompletedTodos>
+    <GetTodo></GetTodo>
+    <CurrentTodos></CurrentTodos>
+  </div>
+</template>
+
+<script>
+import ToDoComp from '@/components/ToDo.vue'
+import GetTodo from '@/components/GetTodo.vue'
+import CurrentTodos from '@/components/CurrentTodos.vue'
+import CompletedTodos from '@/components/CompletedTodos.vue'
+export default {
+  name: 'todoEdit',
+  components: {
+    ToDoComp,
+    GetTodo,
+    CurrentTodos,
+    CompletedTodos
+  }
+}
+</script>
